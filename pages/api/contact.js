e8a183c04174a03c9dd368ad5a9b7259d0158ce4
VladoLov <vladislav.lovric@gmail.com>
2024-06-19 10:44:23 +0200

    second commit

---
+++ pages/api/contact.js
@@ -0,0 +1,29 @@
+import { supabase } from "@/lib/supabase";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST")
+    return res.status(405).json({
+      success: false,
+      message: "Please make a POST request",
+    });
+  const contactData = {
+    fullName: "Jonas",
+    email: "Test",
+    subject: "booking",
+    message: "Hey",
+  };
+
+  const { error } = await supabase.from("contact").insert([contactData]);
+
+  if (error)
+    res.status(500).json({
+      success: false,
+      message: "Could not send your message. Please try again",
+    });
+
+  //success message
+  res.status(200).json({
+    success: true,
+    message: "Thank for your message! We will be in touch soon",
+  });
+}
